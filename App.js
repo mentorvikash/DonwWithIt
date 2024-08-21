0a82b5ea7234552073e99346a100069a9e273c14
mentorvikash <mentorvikash@gmail.com>
2024-08-21 19:52:10 +0530

    learning fundamental conpects

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, TouchableNativeFeedback } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => {
+    alert("You just touched me")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <Text numberOfLines={1}>Wecome to Native App</Text>
+      <Image source={require('./assets/favicon.png')} />
+      <Image fadeDuration={1000} blurRadius={1} source={{ width: 200, height: 300, uri: "https://picsum.photos/200/300" }} />
+      <TouchableOpacity>
+        <Image onPress={() => alert("Hay you touch me")} fadeDuration={1000} blurRadius={1} source={{ width: 200, height: 300, uri: "https://picsum.photos/200/300" }} />
+      </TouchableOpacity>
+      <TouchableHighlight
+        onPress={() => console.log('Pressed!')}
+        underlayColor="#DDDDDD"
+      >
+        <View style={{ padding: 10, backgroundColor: 'blue' }}>
+          <Text style={{ color: 'white' }}>Press Me</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableOpacity
+        onPress={() => console.log('Pressed!')}
+        activeOpacity={0.6}
+      >
+        <View style={{ padding: 10, backgroundColor: 'green' }}>
+          <Text style={{ color: 'white' }}>Press Me</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableNativeFeedback >
+        <View style={{ width: 100, height: 30, backgroundColor: "dodgerblue", blurRadius: "10px" }}>
+          <Text onPress={() => alert("I got clicked")} style={{ textAlign: 'center' }}>
+            Click Me
+          </Text>
+        </View>
+      </TouchableNativeFeedback>
+      <Button title='Submit' onPress={() => console.log("request successfully submitted")} />
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +51,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
